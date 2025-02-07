9abb2c914634137bdac41a268e9cf4f2378f25f1
ahmedguindou <guindouahmed@gmail.com>
2025-02-07 09:19:49 +0100

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a private instance attribute size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size

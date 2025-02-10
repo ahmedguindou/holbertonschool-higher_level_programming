7b3758497cd05dd57a73e109f6e22473fb58a174
ahmedguindou <guindouahmed@gmail.com>
2025-02-10 18:56:20 +0100

    task_05_dragon.py

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,22 @@
-#!/usr/bin/env python3
+class SwimMixin:
+    """Provides swimming ability."""
+
+    def swim(self):
+        """Enables swimming."""
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Provides flying ability."""
+
+    def fly(self):
+        """Enables flying."""
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """A dragon that can swim and fly."""
+
+    def roar(self):
+        """Roars loudly."""
+        print("The dragon roars!")

42b8b31ff2dfed0cbeca7312a6ab5133fdd63ae6
ahmedguindou <guindouahmed@gmail.com>
2025-03-07 03:01:03 +0100

    MORE SQL

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- List all genres of the show Dexter
-SELECT tv_genres.name
+-- List all Comedy shows
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;

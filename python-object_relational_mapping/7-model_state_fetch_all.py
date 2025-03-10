bd11dce7a38059506f97f7b12a595050f4d8646e
ahmedguindou <guindouahmed@gmail.com>
2025-03-10 04:07:52 +0100

    chmod

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
               "<mysql username> <mysql password> <database name>")
         sys.exit(1)
 
-    # DB connection
+    # DB connectionn
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
